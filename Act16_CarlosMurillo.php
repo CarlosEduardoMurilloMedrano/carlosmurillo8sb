374f41374e05e5392d39e71a5079e454a896580f
CarlosEduardoMurilloMedrano <66284174+CarlosEduardoMurilloMedrano@users.noreply.github.com>
2020-06-14 14:26:05 -0500

    Add files via upload

---
+++ Act16_CarlosMurillo.php
@@ -0,0 +1,68 @@
+
+<html>
+  <head>
+  <meta http-equiv= "Content-Type"  charset="utf-8"/>
+  <title>Act16_CarlosMurillo</title>
+  <style type="text/css">
+  .a{
+   color:#00F;
+
+   }
+   td{
+    text-align: center;
+   }
+    </style>
+    </head>
+    
+    <body>
+
+      <div align="center">
+          <h1>Nombre: Carlos Murillo Medrano || Curso: 8SB</h1>
+          <h2>Profesora: Ing. Andrea Sinche</h2>
+        <table width="300" border="2">
+             <tr>
+             <td width="">Acer</td>
+             <td width="68"><a href="https://www.acer.com/ac/es/AR/content/home"><img src="img/acer.jpg" width="68" height="68" > </a></td>
+             <?php $td="<td width='100'>Amarillo</td>
+                        <td bgcolor='#FFFF00' width='100'></td>" ;
+             echo $td;
+             ?>
+             </tr>
+
+             <tr>
+             <td width="54">alienware</td>
+             <td width="68"><a href="https://www.dell.com/es-es/gaming/alienware"><img src="img/alienware.jpg" width="68" height="68" > </a></td>
+               <?php $td1="<td width='100'>Verde</td>
+               <td bgcolor='#00FF7F'></td>" ;
+             echo $td1;
+             ?>
+             </tr>
+
+             <tr>
+             <td width="54">Compaq</td>
+             <td width="68"><a href="https://www8.hp.com/ec/es/ads/todoenuno/compaq.html"><img src="img/compaq.png" width="68" height="68" > </a></td>
+              <?php $td2="<td width='100'>Conchevino</td>
+              <td bgcolor='#930A20'></td>" ;
+             echo $td2;
+             ?>
+             </tr>
+
+             <tr>
+             <td width="54">Gateway</td>
+             <td width="68"><a href="https://www.gateway.com/worldwide/"><img src="img/gateway.png" width="68" height="68" > </a></td>
+             <?php $td3="<td width='100'>Rosado</td>
+             <td bgcolor='#FF00FF'></td>" ;
+             echo $td3;
+             ?>
+             </tr>
+
+             <tr>
+             <td width="54">Lanix</td>
+             <td width="68"><a href="https://lanix.com/"><img src="img/lanix.png" width="68" height="68" > </a></td>
+             <?php $td4="<td width='100'>Azul</td>
+             <td bgcolor='#0000FF'></td>" ;
+             echo $td4;
+             ?>
+             </tr>
+  </body>
+</html>
